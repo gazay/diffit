38cd37c296f9702867f63ee1b50917282e22fb7b
Alexey Gaziev <alex.gaziev@gmail.com>
2015-05-05 07:18:51 +0000

    Typo

--- lib/generators/templates/trigger_template.rb
+++ lib/generators/templates/trigger_template.rb
@@ -3,4 +3,4 @@ function_name = "#{table_name}_#{column_name}"
   AFTER UPDATE OR CREATE OR DELETE OF #{column_name} ON #{table_name}
   FOR EACH ROW
   WHEN (OLD.#{column_name} IS DISTINCT FROM NEW.#{column_name})
-  EXECUTE PROCEDURE diff_#{function_name}_fun();
+  EXECUTE PROCEDURE diff_#{function_name}_fun();|
