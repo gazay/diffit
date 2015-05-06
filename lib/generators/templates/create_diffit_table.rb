843676b04d11bcbdfd09e24e5e61a3ef6340542d
Alexey Gaziev <alex.gaziev@gmail.com>
2015-05-06 18:23:30 +0000

    Rename model_name to table_name in diff table

--- lib/generators/templates/create_diffit_table.rb
+++ lib/generators/templates/create_diffit_table.rb
@@ -2,15 +2,15 @@ class CreateColumnDiffs < ActiveRecord::Migration
   def change
     create_table :column_diffs do |t|
       t.datetime :changed_at
-      t.string   :model_name
+      t.string   :table_name
       t.integer  :record_id
       t.string   :column_name
       t.hstore   :value
     end
 
     add_index :column_diffs, :changed_at
-    add_index :column_diffs, [:changed_at, :model_name]
-    add_index :column_diffs, [:model_name, :record_id]
-    add_index :column_diffs, [:changed_at, :model_name, :record_id]
+    add_index :column_diffs, [:changed_at, :table_name]
+    add_index :column_diffs, [:table_name, :record_id]
+    add_index :column_diffs, [:changed_at, :table_name, :record_id]
   end
 end
