f0732332939c485e8370c5cc7d37ae25ad4ad71d
Denis Lifanov <inadsence@gmail.com>
2015-09-07 16:33:32 +0300

    [drafr] Pass model name to diffit fuction

--- lib/generators/diffit/triggers/triggers_generator.rb
+++ lib/generators/diffit/triggers/triggers_generator.rb
@@ -5,28 +5,21 @@ module Diffit
 
     source_root File.dirname(__FILE__)
 
-    argument :table_name, type: :string, required: true, desc: "ModelName or table_name"
+    argument :model_name, type: :string, required: true, desc: "ModelName or table_name"
 
     desc 'Creates diffit triggers using a ModelName or a table_name provided.'
 
     def create_triggers_migration
-      detect_table_name!
-      basename = "create_#{Diffit.function_name.to_s.underscore}_triggers_on_#{table_name}"
+      detect_model!
+      basename = "create_#{Diffit.function_name.to_s.underscore}_triggers_on_#{@model.table_name}"
       create_diffit_migration 'migrations/create_triggers.erb', basename
     end
 
     protected
 
-    def detect_table_name!
-      return if table_name == table_name.tableize
-
-      begin
-        klass = table_name.classify.constantize
-        self.table_name = klass.table_name if klass.respond_to?(:table_name)
-      rescue NameError
-      end
-
-      nil
+    def detect_model!
+      @model = model_name.classify.constantize # TODO: use class cache?
+      raise unless @model.respond_to?(:table_name)
     end
 
   end
